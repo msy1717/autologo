0769195cda4f29a0b339571b5db17a524fa49f43
msy1717 <80758264+msy1717@users.noreply.github.com>
2021-09-22 19:10:44 +0530

    Create Logo.py

---
+++ Logo.py
@@ -0,0 +1,73 @@
+  
+
+
+os.system("pip install Telethon==1.21.1")
+
+from telethon import TelegramClient, events, functions, types
+
+api_id = os.environ.get("APP_ID")
+
+import os, asyncio
+
+from os import system
+
+api_hash = os.environ.get("API_HASH")
+
+token = os.environ.get("BOT_TOKEN")
+
+client = TelegramClient('mrunal', api_id, api_hash).start(bot_token=token)
+from telethon import events, Button
+import os 
+from PIL import Image, ImageDraw, ImageFont
+import shutil 
+import random, re
+import glob
+import time
+from telethon.tl.types import InputMessagesFilterPhotos
+
+BGS = ["resources/bgsrnd/11.jpg", 
+       "resources/bgsrnd/12.jpg", 
+       "resources/bgsrnd/13.jpg", 
+       "resources/bgsrnd/15.jpg"
+      ]
+ 
+                         
+                         
+
+@client.on(events.NewMessage(incoming=True, pattern="/logo"))
+async def lego(event):
+ quew = event.text.split("",1)[1]
+ #quew = event.text.split[1]
+ if not quew:
+    await event.reply('Provide Some Text To Draw!')
+    return
+ else:
+    pass
+ await event.reply('Creating your logo...wait!')
+ try:
+    text = event.text.split(" ",1)[1]
+    #img = Image.open('./TelethonBot/resources/bgsrnd/15.jpg')
+    #dirr = os.listdir("./TelethonBot/resources/bgsrnd")
+    #rnd = random.choice(dirr)
+    pp = random.choice(BGS)
+    img = Image.open(pp)
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "gold"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("resources/Chopsic.otf", 330)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
+    fname2 = "LogoBy.png"
+    img.save(fname2, "png")
+    await client.send_file(event.chat_id, fname2, caption="Made By MultiFunctionBot")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Report @SouGuu, {e}')
